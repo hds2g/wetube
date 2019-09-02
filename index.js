6fc63f6543f6d4a62cd8bfec533f8d6bc02c1c59
DS Hwang <hds2g22@gmail.com>
2019-09-03 01:15:49 +0900

    Express Core: Middlewares

--- index.js
+++ index.js
@@ -12,10 +12,18 @@ const handleHome = (req, res) => {
   res.send("hello from home");
 };
 
+const betweenHome = (req, res, next) => {
+  console.log("I'm between");
+  next();
+};
+
 function handleProfile(req, res) {
-  res.send("Profile!!!");
+  res.send("Profile!!!!");
 }
 
+app.use(betweenHome);
+
+//app.get("/",betweenHome, handleHome);
 app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
