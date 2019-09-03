8058631d214626cbd9af7bc2cd89774db4f139e9
DS Hwang <hds2g22@gmail.com>
2019-09-03 14:02:21 +0900

    Middlewares part Two: morgan, helmet, body-parser, cookie-parser
    * Morgan(middleware) : logging(무슨 일이 어디서 일어났는지 기록)
     - npm install morgan
     - import morgan from "morgan";
     - app.use(morgan());
     - 인자로 로깅 옵션 설정 가능
       - "tiny", "combined", "common", "dev"
    
    * helmet(middleware) : node.js 앱 보안에 도움
     - npm install helmet
     - import helmet from "helmet"
     - app.use(helmet());
    
    * middleware로 연결(connection) 끊기
     - 미들웨어가 res.send 호출하면 거기서 연결 끝
    
    * cookie parser(middleware) : session을 다루기 위해 cookie에 사용자 정보 저장
     - npm install cookie-parser
     - import cookieParser from "cookie-parser";
     - app.user(cookiePaerser());
    
    * body parser(middleware) : form 데이터 가진 request object에 접근
     - npm install body-parser
     - import bodyParser from "body-parser";
     - app.use(bodyParser());
     - 옵션
       - json : bodyParser.json();
       - html : bodyParser.urlencoded({extended:true});

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 //const express = require("express");
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 
 const PORT = 4000;
@@ -8,20 +13,26 @@ const handleListening = () => {
 };
 
 const handleHome = (req, res) => {
-  console.log(req);
+  //console.log(req);
   res.send("hello from home");
-};
-
-const betweenHome = (req, res, next) => {
-  console.log("I'm between");
-  next();
 };
 
 function handleProfile(req, res) {
   res.send("Profile!!!!");
 }
 
-app.use(betweenHome);
+//app.use(morgan("tiny"));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(helmet());
+
+/*
+const middleware = (req, res, next) => {
+  res.send("not happening"); // disconnet middleware
+};
+*/
 
 //app.get("/",betweenHome, handleHome);
 app.get("/", handleHome);
