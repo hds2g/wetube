1c71f17ee0bb85a61a11796a87b1813e05da5ff3
DS Hwang <hds2g22@gmail.com>
2019-09-10 01:35:03 +0900

    Search Controller Join/Log In HTML Change Profile HTML

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # WeTube
 
 Cloing Youtube with Vanilla and NodeJS
+
+## Pages:
+
+- [x] Home
+- [x] Join
+- [x] Login
+- [x] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload Video
+- [ ] Video Detail
+- [ ] Edit Video
+
+
--- controller/videoController.js
+++ controller/videoController.js
@@ -1,13 +1,35 @@
 export const home = (req, res) => res.render("home", { pageTitle: "Home" });
-export const search = (req, res) =>
-  res.render("search", { pageTitle: "Search" });
+
+export const search = (req, res) => {
+  //const searchingBy = req.query.term;
+
+  // object destructuring
+  /*
+var o = {p: 42, q: true};
+var {p: foo, q: bar} = o;
+
+console.log(foo); // 42
+console.log(bar); // true
+  */
+  const {
+    query: { term: searchingBy }
+  } = req;
+
+  //res.render("search", { pageTitle: "Search", searchingBy: searchingBy });
+  res.render("search", { pageTitle: "Search", searchingBy });
+};
+
 export const videos = (req, res) =>
   res.render("videos", { pageTitle: "Videos" });
+
 export const upload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
+
 export const videoDetail = (req, res) =>
   res.render("videoDetail", { pageTitle: "Video Detail" });
+
 export const editVideo = (req, res) =>
   res.render("editVideo", { pageTitle: "Edit Video" });
+
 export const deleteVideo = (req, res) =>
   res.render("deleteVideo", { pageTitle: "Delete Video" });
