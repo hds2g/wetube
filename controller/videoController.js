190595cbc242d920a8de55dd5691874aacef080a
DS Hwang <hds2g22@gmail.com>
2019-09-16 23:35:23 +0900

    Delete Video

--- controller/videoController.js
+++ controller/videoController.js
@@ -59,7 +59,7 @@ export const videoDetail = async (req, res) => {
   try {
     const video = await Video.findById(id);
     console.log(video);
-    res.render("videoDetail", { pageTitle: "Video Detail", video });
+    res.render("videoDetail", { pageTitle: video.title, video });
   } catch (error) {
     console.log(error);
     res.redirect(route.home);
@@ -92,5 +92,13 @@ export const postEditVideo = async (req, res) => {
   }
 };
 
-export const deleteVideo = (req, res) =>
-  res.render("deleteVideo", { pageTitle: "Delete Video" });
+export const deleteVideo = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+
+  try {
+    await Video.findOneAndRemove({ _id: id });
+  } catch (error) {}
+  res.redirect(routes.home);
+};
