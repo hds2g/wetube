ce66e796bfdeb714169bc5d0266838b5756fe8db
DS Hwang <hds2g22@gmail.com>
2019-09-18 01:31:01 +0900

    Styles with Webpack2

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const app = express();
 app.set("view engine", "pug");
 
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 //app.use(morgan("tiny"));
 app.use(helmet());
 app.use(cookieParser());
