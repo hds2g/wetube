b8b83cff780e5ba8d8d852a0e843a3702799531a
DS Hwang <hds2g22@gmail.com>
2019-09-23 22:18:18 +0900

    Add onlyPrivate, onlyPublic middlewares

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -6,11 +6,12 @@ import {
   editProfile,
   changePassword
 } from "../controller/userController";
+import { onlyPrivate } from "../middlewares";
 
 export const userRouter = express.Router();
 
-userRouter.get(routes.editProfile, editProfile); // have to be over userDetail
-userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.editProfile, onlyPrivate, editProfile); // have to be over userDetail
+userRouter.get(routes.changePassword, onlyPrivate, changePassword);
 userRouter.get(routes.userDetail(), userDetail);
 
 export default userRouter;
