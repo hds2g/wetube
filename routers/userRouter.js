dbe97f538426861da81a8f608cbc23e58ca4fcb5
DS Hwang <hds2g22@gmail.com>
2019-09-25 07:38:08 +0900

    User Profile to be more sexy

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -3,14 +3,14 @@ import routes from "../routes";
 
 import {
   userDetail,
-  editProfile,
-  changePassword
+  changePassword,
+  getEditProfile
 } from "../controller/userController";
 import { onlyPrivate } from "../middlewares";
 
 export const userRouter = express.Router();
 
-userRouter.get(routes.editProfile, onlyPrivate, editProfile); // have to be over userDetail
+userRouter.get(routes.editProfile, onlyPrivate, getEditProfile); // have to be over userDetail
 userRouter.get(routes.changePassword, onlyPrivate, changePassword);
 userRouter.get(routes.userDetail(), userDetail);
 
