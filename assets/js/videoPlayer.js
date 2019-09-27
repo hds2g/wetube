fbc91f6932b099fb9ad6a2fdf227493d49ece62e
DS Hwang <hds2g22@gmail.com>
2019-09-27 23:50:25 +0900

    Custom Video Player #4 - Full Screen

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -2,6 +2,7 @@ const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayBtn");
 const volumeBtn = document.getElementById("jsVolumeBtn");
+const fullScreenBtn = document.getElementById("jsFullScreen");
 
 function handlePlayClick() {
   if (videoPlayer.paused) {
@@ -13,6 +14,20 @@ function handlePlayClick() {
   }
 }
 
+function exitFullScreen() {
+  fullScreenBtn.innerHTML = `<i class="fas fa-expand"></i>`;
+  fullScreenBtn.removeEventListener("click", exitFullScreen);
+  fullScreenBtn.addEventListener("click", goFullScreen);
+  document.webkitExitFullscreen();
+}
+
+function goFullScreen() {
+  videoContainer.webkitRequestFullscreen();
+  fullScreenBtn.innerHTML = `<i class="fas fa-compress"></i>`;
+  fullScreenBtn.removeEventListener("click", goFullScreen);
+  fullScreenBtn.addEventListener("click", exitFullScreen);
+}
+
 function handleVolumeClick() {
   if (videoPlayer.muted) {
     videoPlayer.muted = false;
@@ -22,10 +37,10 @@ function handleVolumeClick() {
     volumeBtn.innerHTML = `<i class="fas fa-volume-mute"></i>`;
   }
 }
-
 function init() {
   playBtn.addEventListener("click", handlePlayClick);
   volumeBtn.addEventListener("click", handleVolumeClick);
+  fullScreenBtn.addEventListener("click", goFullScreen);
 }
 
 if (videoContainer) {
