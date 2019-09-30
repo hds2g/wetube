d212e43747c7857681569a709c6b1a03169df383
DS Hwang <hds2g22@gmail.com>
2019-10-01 01:12:09 +0900

    API Adding a Comment #1

--- routes.js
+++ routes.js
@@ -33,6 +33,7 @@ const FB_CALLBACK = "/auth/facebook/callback";
 
 const API = "/api";
 const REGISTER_VIEW = "/:id/view";
+const ADD_COMMENT = "/:id/comment";
 
 const routes = {
   home: HOME,
@@ -77,7 +78,8 @@ const routes = {
   facebook: FB,
   facebookLoginCallback: FB_CALLBACK,
   api: API,
-  registerView: REGISTER_VIEW
+  registerView: REGISTER_VIEW,
+  addComment: ADD_COMMENT
 };
 
 export default routes;
